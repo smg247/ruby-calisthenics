d43319ae5cdb82e82208d45624fda2027f81b823
Stephen Goeddel <sgoeddel247@gmail.com>
2017-09-08 20:50:26 +0000

    couple of the fun with strings completed.

--- lib/fun_with_strings.rb
+++ lib/fun_with_strings.rb
@@ -1,10 +1,31 @@
 module FunWithStrings
   def palindrome?
-    # your code here
+    return self.sub_non_word_chars.reverse.upcase == self.sub_non_word_chars.upcase
   end
+  
+  def sub_non_word_chars
+    return self.gsub(/\W|\d/, "")
+  end
+
   def count_words
-    # your code here
+    count = {}
+    words = tokenize
+    words.each { |word|
+      word.downcase!
+      if count.has_key? word then
+        count[word] = count[word] + 1
+      else
+        count[word] = 1
+      end
+    }
+    
+    return count
   end
+  
+  def tokenize
+    return self.gsub(/\W/, ' ').split(" ")
+  end
+  
   def anagram_groups
     # your code here
   end
