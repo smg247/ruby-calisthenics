0abb9e340fb94fbb002e23afc18929c46d886542
Stephen Goeddel <sgoeddel247@gmail.com>
2017-09-10 14:38:16 +0000

    desserts done.

--- lib/dessert.rb
+++ lib/dessert.rb
@@ -1,17 +1,25 @@
 class Dessert
+  attr_accessor :name, :calories
+  
   def initialize(name, calories)
-    # your code here
+    @name = name
+    @calories = calories
   end
   def healthy?
-    # your code here
+    return @calories < 200
   end
   def delicious?
-    # your code here
+    return true
   end
 end
 
 class JellyBean < Dessert
   def initialize(flavor)
-    # your code here
+    @flavor = flavor
+    super(flavor + " jelly bean", 5)
+  end
+  
+  def delicious?
+    return @flavor != "licorice"
   end
 end
