29fe75613ac0718874e373164495ccd5d156ab35
Stephen Goeddel <sgoeddel247@gmail.com>
2017-09-10 15:32:50 +0000

    metaprogramming done.

--- lib/attr_accessor_with_history.rb
+++ lib/attr_accessor_with_history.rb
@@ -4,7 +4,22 @@ class Class
     attr_reader attr_name # create the attribute's getter
     attr_reader attr_name+"_history" # create bar_history getter
     class_eval %Q{
-      # YOUR CODE HERE
+      def #{attr_name}=(value)
+        # lazy initialization
+        if @history_#{attr_name} == nil then
+          @history_#{attr_name} = []
+        end
+        
+        # push the old value onto the history
+        @history_#{attr_name}.push(#{attr_name})
+        
+        # finally actually set the value
+        @#{attr_name} = value
+      end
+      
+      def #{attr_name}_history
+        return @history_#{attr_name}
+      end
     }
   end
 end
